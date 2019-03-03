14ec74ae0d4775d7866bd9c3c439f46af128062a
norbix93 <norbix93@gmail.com>
2019-03-03 16:38:41 +0100

    add new files

---
+++ backend-specialization/bew2/ex3-thecursedcoin/src/main/java/at/nacs/ex3thecursedcoin/JackSparrow.java
@@ -0,0 +1,11 @@
+package at.nacs.ex3thecursedcoin;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JackSparrow {
+
+    public String findCursedCoin() {
+        return "I found the CursedCoin!";
+    }
+}
