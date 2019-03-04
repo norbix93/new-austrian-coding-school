611f605679347b5244d47e06b36f0cb7035b6142
norbix93 <norbix93@gmail.com>
2019-03-04 14:24:19 +0100

    commit changes

---
+++ backend-specialization/bew3/ex6-properties_p1/src/main/java/at/nacs/ex6properties_part1/Ex6PropertiesPart1Application.java
@@ -0,0 +1,13 @@
+package at.nacs.ex6properties_part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex6PropertiesPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex6PropertiesPart1Application.class, args);
+    }
+
+}
