238d1bc4db24e844deebf84b689f9b499fac78f2
norbix93 <norbix93@gmail.com>
2019-03-05 08:01:48 +0100

    add changes

---
+++ backend-specialization/bew3/ex6-properties_p1/src/test/java/at/nacs/ex6properties_p1/WordTest.java
@@ -0,0 +1,12 @@
+package at.nacs.ex6properties_p1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordTest {
+
+    @Test
+    void getMeaning() {
+    }
+}
