4d94e3e428bea990aa41ce70e98cd44e0ad08da9
norbix93 <norbix93@gmail.com>
2019-03-20 11:13:23 +0100

    update tests

--- backend-specialization/bew4/reflection-morse_code/morse_code_server/src/main/java/at/nacs/morse_code_server/controller/Dictionary.java
+++ backend-specialization/bew4/reflection-morse_code/morse_code_server/src/main/java/at/nacs/morse_code_server/controller/Dictionary.java
@@ -2,8 +2,10 @@ package at.nacs.morse_code_server.controller;
 
 import at.nacs.morse_code_server.model.Entry;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,7 +17,6 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 public class Dictionary {
-
     private List<Entry> entry;
 
     public Map<String, String> createMap() {
