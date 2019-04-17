b893f9100d8dafbbba0554ae3144d30f5855c77f
norbix93 <norbix93@gmail.com>
2019-04-17 13:38:13 +0200

    commit changes

--- backend-specialization/bew7/drhouse-accountancy/src/main/java/at/nacs/drhouse_accountancy/persistence/invoice/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package at.nacs.drhouse_accountancy.persistence.invoice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
-}
