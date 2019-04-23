96b82266ea278bde26a9e239c51077b9340f86e1
norbix93 <norbix93@gmail.com>
2019-04-23 15:29:07 +0200

    add new project

---
+++ backend-specialization/bew10/ex1-the-thieves-den/src/main/java/at/nacs/thethievesden/TheThievesDenApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.thethievesden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheThievesDenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheThievesDenApplication.class, args);
+    }
+
+}
